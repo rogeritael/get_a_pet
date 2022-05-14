3b6eb53463ce75a7c5ae29550bc790eec1fbc5ef
rogeritael <rogeritael.contato@gmail.com>
2022-05-14 06:43:16 -0300

    terminando atualização de usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -153,18 +153,31 @@ class UserController
             return;
         }
 
+        user.email = email;
+
         if(!phone){
             res.status(422).json({ message: 'O telefone é obrigatório' });
             return;
         }
 
-        if(!password){
-            res.status(422).json({ message: 'A senha é obrigatória' });
+        user.phone = phone;
+
+        if(password != confirmpassword){
+            res.status(422).json({ message: 'As senhas não conferem' });
             return;
+        }else if(password === confirmpassword && password !== null){
+            const salt = await bcrypt.genSalt(12);
+            const passwordHash = await bcrypt.hash(password, salt);
+
+            user.password = passwordHash;
         }
 
-        if(!confirmpassword){
-            res.status(422).json({ message: 'A confirmação de senha é obrigatória' });
+        try {
+            
+            const updateduser = await User.findByIdAndUpdate({ _id: user._id }, { $set: user }, { new: true })
+            res.status(200).json({ message: 'Usuário atualizado com sucesso' });
+        } catch (error) {
+            res.status(500).json({ message: error });
             return;
         }
 
