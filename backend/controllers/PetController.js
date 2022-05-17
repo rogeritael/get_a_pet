90ca6ef6812814e9c7e73e75b2dd733613cf9099
rogeritael <rogeritael.contato@gmail.com>
2022-05-17 08:29:22 -0300

    atualização de pet

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -114,6 +114,7 @@ class PetController {
         const pet = await Pet.findOne({ _id: id });
         if(!pet){
             res.status(404).json({ message: 'Pet não encontrado' });
+            return;
         }
 
         const token = req.headers['x-access-token'];
@@ -121,12 +122,75 @@ class PetController {
 
         if(pet.user._id.toString() !== user._id.toString()){
             res.status(422).json({ message: 'Houve um problema em precessar a sua solicitação. Tente novamente mais tarde' });
-
+            return;
         }
 
         await Pet.findByIdAndRemove(id);
         res.status(200).json({ message: 'Pet removido com sucesso' });
-        
+    }
+
+    static async updatePet(req, res){
+        const { id } = req.params;
+        const { name, age, weight, color } = req.body;
+        const images = req.files;
+        const updatedData = {};
+
+        const pet = await Pet.findOne({ _id: id });
+        if(!pet){
+            res.status(404).json({ message: 'Pet não encontrado' });
+            return;
+        }
+
+        const token = req.headers['x-access-token'];
+        const user = await getUserByToken(token);
+
+        if(pet.user._id.toString() !== user._id.toString()){
+            res.status(422).json({ message: 'Houve um problema em precessar a sua solicitação. Tente novamente mais tarde' });
+            return;
+        }
+
+        // validation
+        if(!name){
+            res.status(422).json({ message: 'O nome é obrigatório' });
+            return;
+        }else{
+            updatedData.name = name;
+        }
+
+        if(!weight){
+            res.status(422).json({ message: 'O peso é obrigatório' });
+            return;
+        }else{
+            updatedData.weight = weight;
+        }
+
+        if(!age){
+            res.status(422).json({ message: 'A idade é obrigatória' });
+            return;
+        }else{
+            updatedData.age = age;
+        }
+
+        if(!color){
+            res.status(422).json({ message: 'A cor é obrigatória' });
+            return;
+        }else{
+            updatedData.color = color;
+        }
+
+        if(images.length === 0){
+            res.status(422).json({ message: 'A imagem é obrigatória' });
+            return;
+        }else{
+            updatedData.images = [];
+            images.map((image) => {
+                updatedData.images.push(image.filename);
+            });
+        }
+
+
+        await Pet.findByIdAndUpdate(id, updatedData);
+        res.status(200).json({ message: 'Pet atualizado com sucesso' });
     }
 }
 
