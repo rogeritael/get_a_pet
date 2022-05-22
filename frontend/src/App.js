edcaf9589f6607282c6dcebc277ada88645009fe
rogeritael <rogeritael.contato@gmail.com>
2022-05-22 19:16:06 -0300

    criando as flash messages de cadastro de usuário

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/pages/Auth/Login';
 import Register from './components/pages/Auth/Register';
 import Home from './components/pages/Home';
-import Container from './components/layout/Container'
+
 
 //components
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Container from './components/layout/Container';
+import Message from './components/layout/Message';
 
 // context
 import { UserProvider } from './context/UserContext';
@@ -16,6 +18,7 @@ function App() {
     <Router>
       <UserProvider>
         <Navbar/>
+          <Message />
           <Container>
             <Routes>
               <Route path="/login" element={<Login />} />
--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -3,9 +3,16 @@ import api from '../utils/api';
 
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import useFlashMessage from './useFlashMessage';
 
 export default function useAuth(){
+    
+    const { setFlashMessage } = useFlashMessage();
+
     async function register(user){
+        let msgText = 'Cadastro realizado com sucesso';
+        let msgType = 'success';
+
         try {
             const data = await api.post('/users/register', user).then((res) => {
                 return res.data;
@@ -13,9 +20,13 @@ export default function useAuth(){
 
             console.log(data)
         } catch (error) {
-            console.log(error)
+            msgText = error.response.data.message;
+            msgType = 'error';
         }
+
+        setFlashMessage(msgText, msgType);
     }
+
 
     return { register }
 }
