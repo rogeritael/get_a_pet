3e35bc0ab2327d7241f798d1e1285d1c855b0538
rogeritael <rogeritael.contato@gmail.com>
2022-05-23 00:37:09 -0300

    criando o formulário de login

--- frontend/src/components/pages/Auth/Login.js
+++ frontend/src/components/pages/Auth/Login.js
@@ -1,7 +1,39 @@
+import {useState, useContext} from 'react';
+import Input from '../../form/Input';
+import { Link } from 'react-router-dom';
+
+import styles from '../../form/Form.module.css';
+
+import { Context } from '../../../context/UserContext';
+
 function Login(){
+    function handleChange(){
+
+    }
+
     return (
-        <section>
+        <section className={styles.form_container}>
             <h1>Login</h1>
+            <form>
+                <Input
+                    text='E-mail'
+                    type='email'
+                    name='email'
+                    placeholder='Digite o seu e-mail'
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text='Senha'
+                    type='password'
+                    name='password'
+                    placeholder='Digite a sua senha'
+                    handleOnChange={handleChange}
+                />
+                <input type="submit" value="entrar"/>
+            </form>
+            <p>
+                Não tem conta? <Link to="/register">crie agora</Link>
+            </p>
         </section>
     );
 }
