7ca18d67244bb49bcb72a1bb30e5665d780eb64b
rogeritael <rogeritael.contato@gmail.com>
2022-05-31 05:24:01 -0300

    criando estilos de footer e header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
+import { Navbar } from './components/layout/Navbar';
+import { Footer } from './components/layout/Footer';
+
 import {Home} from './components/pages/Home';
 import {Login} from './components/pages/Auth/Login';
 import {Register} from './components/pages/Auth/Register';
@@ -7,11 +10,13 @@ import {Register} from './components/pages/Auth/Register';
 export default function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/register" element={<Register/>} />
         <Route path="/login" element={<Login/>} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
