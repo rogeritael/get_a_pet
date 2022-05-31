d020fa050019f6accffdcd462be48a486c13c21e
rogeritael <rogeritael.contato@gmail.com>
2022-05-31 09:18:26 -0300

    criando o formulário de cadastro de usuário

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import { Navbar } from './components/layout/Navbar';
 import { Footer } from './components/layout/Footer';
+import {Container} from './components/layout/Container';
 
 import {Home} from './components/pages/Home';
 import {Login} from './components/pages/Auth/Login';
@@ -11,11 +12,13 @@ export default function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/login" element={<Login/>} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="/login" element={<Login/>} />
+        </Routes>
+      </Container>
       <Footer />
     </Router>
   );
--- frontend/src/components/pages/Auth/Register.jsx
+++ frontend/src/components/pages/Auth/Register.jsx
@@ -1,7 +1,63 @@
+import { Input } from "../../form/Input";
+import styles from '../../form/Form.module.css';
+import {Link} from 'react-router-dom';
+
 export function Register(){
+    function handleChange(e){
+
+    }
+
+
     return(
-        <section>
-            <h1>Register</h1>
+        <section className={styles.form_container}>
+            <form>
+                <Input
+                    text="Nome"
+                    type="text"
+                    name="name"
+                    placeholder="Digite o seu nome"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Telefone"
+                    type="text"
+                    name="phone"
+                    placeholder="Digite o seu telefone"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="E-mail"
+                    type="email"
+                    name="email"
+                    placeholder="Digite o seu email"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Nome"
+                    type="text"
+                    name="name"
+                    placeholder="Digite o seu nome"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Senha"
+                    type="password"
+                    name="password"
+                    placeholder="Digite a sua senha"
+                    handleOnChange={handleChange}
+                />
+                <Input
+                    text="Confirmação de Senha"
+                    type="password"
+                    name="confirmpassword"
+                    placeholder="Confirme a sua senha"
+                    handleOnChange={handleChange}
+                />
+                <input type="submit" value="Cadastrar" />
+            </form>
+            <p>
+                Já tem conta? <Link to="/login">clique aqui</Link>
+            </p>
         </section>
     )
 }
